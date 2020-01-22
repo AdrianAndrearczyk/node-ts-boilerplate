8952bbd9754f849e7e2d9f88e4daa0e7f84e7151
AdrianAndrearczyk <praca.andrearczyk@gmail.com>
2020-01-22 21:26:02 +0100

    Added body parser, eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser = require('body-parser');
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.SERVER_PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get('/', (req, res) => {
+  res.send('Hello world');
 });
 
 app.listen(port, () => {
